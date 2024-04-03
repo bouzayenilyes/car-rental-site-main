aa0b56fc24e7b4f430fcaad5673cfe7be0796568
bouzayenilyes <ilyesbouzayen@hotmail.com>
2024-04-03 13:07:42 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+## How to install this project
+
+you need xampp/lampp to run this project .
+create a db named `carrental` and import the sql file in your myphpadmin
+<br/>
+> :warning: **the sql file is in the db directory**
+
+
+
+**Admin**
+
+    admin
+    Test@12345
+
+**Client**
+
+       register another user and login
+
+## URL links
+
+| Pages       | URL                                  |
+| ----------- | ------------------------------------ |
+| client page | http://localhost/carrental/index.php |
+| admin page  | http://localhost/carrental/admin/    |
+
+
+## Bugs
+**admin bugs**
+* delete / edit a car
+
+    `manage-vehicles.php`
+
+**client bugs**
+
+- image display
+  `i momentarily fixed this by using images of same size`
+- more bugs coming
+
+**missing module**
+
+- multi-user login
+
+> :warning: **i have updated the database**: find the sql file in the db folder!
+
+## Screenshots
+<br/><br/>
+<img align="right" alt="dash" src="assets/images/screenshot1.png" height="500"/>
+<br/>
+<br/>
+<img align="right" alt="dash" src="assets/images/screenshot2.png" height="500"/> 
+    
+
---
+++ gallery.php
@@ -0,0 +1,127 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>Car Rental Portal | Car Listing</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- SWITCHER -->
+		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+        
+<!-- Fav and touch icons -->
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+
+<!-- Start Switcher -->
+<!-- <?php include('includes/colorswitcher.php');?> -->
+<!-- /Switcher -->  
+
+<!--Header--> 
+<?php include('includes/header.php');?>
+<!-- /Header --> 
+
+<!--Page Header-->
+<section class="page-header listing_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>Gallery</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>Gallery</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<!-- /Page Header--> 
+
+<!--Listing-->
+<section class="listing-page">
+  <div class="container">
+  <div class="card mb-3">
+  <div class="card-body">
+    <h5 class="card-title">We also provide sports cars!!!</h5>
+    <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ut modi aperiam dolore tempora dicta, quis veritatis corporis, labore asperiores minima consequuntur voluptate mollitia! Adipisci magni distinctio commodi necessitatibus numquam?.</p>
+   <iframe width="900" height="345" src="https://www.youtube.com/embed/sNFeU34wL0M">
+</iframe>
+  </div>
+  <div class="container">
+  <div class="card mb-3">
+  <div class="card-body">
+    <h5 class="card-title">vissit our youtube channel to see more</h5>
+    <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ut modi aperiam dolore tempora dicta, quis veritatis corporis, labore asperiores minima consequuntur voluptate mollitia! Adipisci magni distinctio commodi necessitatibus numquam?.</p>
+   <iframe width="900" height="345" src="https://www.youtube.com/embed/RL4bvlgZSyY">
+</iframe>
+
+  </div>
+  </div>
+</section>
+<!-- /Listing--> 
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+</html>
